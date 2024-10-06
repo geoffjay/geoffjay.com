1a58ecf5478c39f977bd032cf888b5950039e398
Geoff Johnson <geoff.jay@gmail.com>
2024-10-06 14:35:15 -0700

    feat: add auth

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "arrow-parens": "error",
+      "comma-dangle": ["error", "always-multiline"],
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-console": "error",
+      "jsx-quotes": ["error", "prefer-double"],
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
     },
   },
-)
+];
