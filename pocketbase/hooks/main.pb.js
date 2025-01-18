9e258878812f4bd122c9548b284b4b463fb47912
Geoff Johnson <geoff.jay@gmail.com>
2025-01-18 13:08:16 -0800

    fix: simplify pb middleware

--- pocketbase/hooks/main.pb.js
+++ pocketbase/hooks/main.pb.js
@@ -1,14 +1,19 @@
-routerUse((next) => {
-  return (c) => {
-    // ip:port string (not sure if the below will work on ipv6 addresses)
-    const addr = c.request().remoteAddr;
-    const ip = addr.substring(0, addr.lastIndexOf(":"));
+// routerUse((next) => {
+//   return (c) => {
+//     // ip:port string (not sure if the below will work on ipv6 addresses)
+//     const addr = c.request().remoteAddr;
+//     const ip = addr.substring(0, addr.lastIndexOf(":"));
+// 
+//     if (ip === process.env.ADMIN_IP) {
+//       console.log("IP check succeeded");
+//       // throw new ForbiddenError("You are not allowed to access this resource");
+//     }
+// 
+//     return next(c);
+//   };
+// });
 
-    if (ip === process.env.ADMIN_IP) {
-      console.log("IP check succeeded");
-      // throw new ForbiddenError("You are not allowed to access this resource");
-    }
-
-    return next(c);
-  };
-});
+routerUse((e) => {
+  console.log(e.request.url.path)
+  return e.next()
+})
