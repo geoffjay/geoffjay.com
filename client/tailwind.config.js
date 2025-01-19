9c958623f09a987675c611818af1c5d2c0800c5d
Geoff Johnson <geoff.jay@gmail.com>
2025-01-19 13:31:00 -0800

    feat: add footer

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,html,json}"],
+  content: ["./src/**/*.{js,html,json,svg}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
