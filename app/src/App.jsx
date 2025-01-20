8b8b4d3db405b606bd284dfba4723ebaf232f2f1
Geoff Johnson <geoff.jay@gmail.com>
2025-01-20 09:38:34 -0800

    feat(app): add fontawesome

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPersonDigging } from "@fortawesome/free-solid-svg-icons";
+
 const App = () => {
   return (
-    <>
-      <div className="text-slate-800">
-        <h1 className="text-3xl font-bold">app</h1>
+    <div className="flex flex-col h-screen text-slate-800">
+      <div className="flex justify-center items-center h-screen">
+        <FontAwesomeIcon icon={faPersonDigging} size="4x" />
       </div>
-    </>
+    </div>
   );
 };
 
