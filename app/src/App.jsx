d35bc6f80b83c3da5c48da06091522097fa5ffd4
Geoff Johnson <geoff.jay@gmail.com>
2025-01-20 09:33:22 -0800

    feat(app): add base application

---
+++ app/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="text-slate-800">
+        <h1 className="text-3xl font-bold">app</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
