e56366a08e81b5ef9608d93d845122077de36c9a
Geoff Johnson <geoff.jay@gmail.com>
2025-01-21 10:24:31 -0800

    feat(app): add shadcn

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPersonDigging } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import { Button } from "@/components/ui/button";
 
 const App = () => {
   return (
     <div className="flex flex-col h-screen text-slate-800">
       <div className="flex justify-center items-center h-screen">
-        <FontAwesomeIcon icon={faPersonDigging} size="4x" />
+        <div className="flex flex-col gap-4">
+          <FontAwesomeIcon icon={faPersonDigging} size="4x" />
+          <Button>
+            <div className="flex gap-2">
+              <FontAwesomeIcon icon={faGithub} />
+              <p>Login with GitHub</p>
+            </div>
+          </Button>
+        </div>
       </div>
     </div>
   );
